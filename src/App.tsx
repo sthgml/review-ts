81643ba17c2b809ab095502ab275efd163c2935e
박소희 <sthgml@naver.com>
2024-04-28 15:59:29 +0900

    chore: App.tsx 초기설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { ThemeProvider } from 'styled-components';
+
+import { Reset } from 'styled-reset';
+
+import routes from './routes';
+
+import GlobalStyle from './styles/GlobalStyle';
+import DefaultTheme from './styles/defaultTheme';
+
+const router = createBrowserRouter(routes);
 
 export default function App() {
-	return (
-		<div>Hello, world!</div>
-	);
+  return (
+    <ThemeProvider theme={DefaultTheme}>
+      <Reset />
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
